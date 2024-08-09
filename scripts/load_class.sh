428a94d6ace2f42de656b74d466697b728259b49
fgalizzi <fedegal99@gmail.com>
2024-08-09 17:29:00 +0200

    A more friendly load_class.sh : )

--- scripts/load_class.sh
+++ scripts/load_class.sh
@@ -1,2 +1,3 @@
+BASE_PATH="/Users/federico/pds-ana"
 echo -e "\n \n Running the general class Analyzer \n \n"
-eval 'root -e "#include \"const.hpp\" " -e "std::string classe_path = \"/Users/federico/pds-ana/Class\"" -e "#include \"/Users/federico/pds-ana/Class/classe.hpp\" " -e "auto a = cla()" '
+eval 'root -e "#include \"const.hpp\" " -e "std::string classe_path = \"$BASE_PATH\" " -e "#include \"$BASE_PATH/Class/classe.hpp\" " -e "auto a = cla()" '
