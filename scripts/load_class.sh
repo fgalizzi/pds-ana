83783cd1763174cd6c861986e5fe3cc2a30d012b
fgalizzi <fedegal99@gmail.com>
2024-10-14 12:35:32 +0200

    Update load_class.sh

--- scripts/load_class.sh
+++ scripts/load_class.sh
@@ -1,3 +1,3 @@
 BASE_PATH="/Users/federico/pds-ana"
 echo -e "\n \n Running the general class Analyzer \n \n"
-eval 'root -e "#include \"const.hpp\" " -e "std::string classe_path = \"$BASE_PATH\" " -e "#include \"$BASE_PATH/Class/classe.hpp\" " -e "auto a = cla()" '
+eval 'root -e "#include \"const.hpp\" " -e "std::string classe_path = \"$BASE_PATH\" " -e "#include \"$BASE_PATH/Class/_c/class_include.hpp\" " -e "auto a = cla()" '
