3ff326fbc13a7c99feaf985e8175e8afc0a023d1
fgalizzi <fedegal99@gmail.com>
2026-04-01 15:55:30 +0200

    TGraph in Optimize_Integration_Window.cpp

--- Class/_c/Optimize_Integration_Window.cpp
+++ Class/_c/Optimize_Integration_Window.cpp
@@ -16,11 +16,19 @@ void cla::Optimise_Integration_Window(size_t min_int_up, size_t max_int_up, size
     }
   }
 
+  TGraph* graph = new TGraph();
+
   for(auto& tuple : tuple_intup_snr){
     cout << "int_up = " << tuple.first << " SNR = " << tuple.second << endl;
+    graph->SetPoint(graph->GetN(), tuple.first, tuple.second);
   }
 
   cout << "Best int_up and SNR" << endl;
   cout << "int_up = " << best_int_up << " SNR = " << best_snr << endl;
 
+  TCanvas* snr_vs_intup = new TCanvas("snr_vs_intup ","snr_vs_intup ",0,0,800,600);
+  snr_vs_intup->cd();
+  graph->SetTitle("SNR vs int_up;int_up;SNR");
+  graph->Draw();
+  snr_vs_intup->Modified(); snr_vs_intup->Update();
 }
